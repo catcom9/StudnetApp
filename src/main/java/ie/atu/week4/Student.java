4be5725d8bdb3e8f7f321791a96ccec167793cb4
Cathal Commins <G00427615@atu.ie>
2024-10-07 09:14:58 +0100

    Created the student class

--- src/main/java/ie/atu/week4/Student.java
+++ src/main/java/ie/atu/week4/Student.java
@@ -1,4 +1,56 @@
 package ie.atu.week4;
 
 public class Student {
+    private String name;
+    private String email;
+    private String course;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public Student(String name, String email, String course) {
+        this.name = name;
+        this.email = email;
+        this.course = course;
+    }
+
+    public Student() {
+        this.name = null;
+        this.email = null;
+        this.course = null;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", course='" + course + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
 }
